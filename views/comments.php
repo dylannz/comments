38909f5135268a58aaa6378797abe0b486067646
Dylan Jennings <dylan@dt.net.nz>
2014-04-23 09:37:38 +1200

    Started comments view

---
+++ views/comments.php
@@ -0,0 +1,12 @@
+<?php if (!defined('IN_APP')) die(); ?>
+
+<div id="comments-list">
+	
+</div>
+
+<div id="new-comment">
+	<form id="new-comment-form" action="">
+		<h2>New comment</h2>
+		
+	</form>
+</div>
