3fc364553a9a903668369d8c9ef82c2a888792c9
Dylan Jennings <dylan@dt.net.nz>
2014-04-23 09:36:36 +1200

    Simple config loading, routing, and view system

---
+++ lib/app.php
@@ -0,0 +1,51 @@
+<?php
+
+class App {
+	
+	public function __construct() {
+		global $config;
+		$this->config = $config;
+		
+		$this->db = new BlackliteDB(
+			$this->config['DB_HOST'],
+			$this->config['DB_USER'],
+			$this->config['DB_PASS'],
+			$this->config['DB_NAME']
+		);
+		
+		$this->root = dirname(dirname(__FILE__));
+		
+		$this->view = 'comments';
+		$this->layout = 'index';
+		$this->action = _get('action', 'index');
+		
+		// If action is not found, die with 404 error
+		if (preg_match('/^_/', $this->action) || !method_exists($this, $this->action)) {
+			header('HTTP/1.0 404 Not Found');
+			echo '<h1>404 not found</h1>';
+			die();
+		}
+		
+		call_user_func(array($this, $this->action));
+	}
+	
+	public function _view() {
+		include($this->root . '/views/' . $this->view . '.php');
+	}
+	
+	public function _href($extra = null) {
+		return $this->config['URL'] . $extra;
+	}
+	
+	private function _404() {
+		
+	}
+	
+	private function index() {
+		
+	}
+	
+}
+
+global $app;
+$app = new App();
