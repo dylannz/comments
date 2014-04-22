93a888c38f074d06ce20927dab8746600106824a
Dylan Jennings <dylan@dt.net.nz>
2014-04-23 09:34:50 +1200

    Added basic configuration

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$config = array(
+	'DB_HOST' => '10.1.1.190',
+	'DB_USER' => 'root',
+	'DB_PASS' => 'xsw2zaq1',
+	'DB_NAME' => 'comments',
+	'URL' => 'http://local.thechurch.dev/comments/'
+);
