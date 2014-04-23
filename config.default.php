816d9ef9ab4b8620ddc1844cba1a99e49e8f1cc5
Dylan Jennings <dylan.jennings@designworks.co.nz>
2014-04-24 09:26:09 +1200

    Sample configuration file

---
+++ config.default.php
@@ -0,0 +1,8 @@
+<?php
+$config = array(
+	'DB_HOST' => '',
+	'DB_USER' => '',
+	'DB_PASS' => '',
+	'DB_NAME' => '',
+	'URL' => 'http://url-of-app-goes-here-with-NO-trailing-slash/a-subdirectory'
+);
