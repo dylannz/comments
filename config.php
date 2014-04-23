ff089f865634018647fc23bc0953a46a13015822
Dylan Jennings <dylan@dt.net.nz>
2014-04-23 22:03:55 +1200

    Removed trailing slash from URL

--- config.php
+++ config.php
@@ -4,5 +4,5 @@ $config = array(
 	'DB_USER' => 'root',
 	'DB_PASS' => 'xsw2zaq1',
 	'DB_NAME' => 'comments',
-	'URL' => 'http://local.thechurch.dev/comments/'
+	'URL' => 'http://local.thechurch.dev/comments'
 );
